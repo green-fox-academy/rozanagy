d398afb2a29934bca141489c5d72816343e56cc4
Róza <rozanagy88@gmail.com>
2019-04-08 17:15:00 +0200

    uploaded

--- week-01/day-3/src/Bmi.java
+++ week-01/day-3/src/Bmi.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+public class Bmi {
+    public static void main(String[] args) {
+        double massInKg = 81.2;
+        double heightInM = 1.78;
 
-public class bmi {
+        System.out.println(massInKg / (heightInM * heightInM));
+    }
 }
+
+
+// Print the Body mass index (BMI) based on these values
