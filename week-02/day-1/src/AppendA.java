051d1b5423fa65f00b3b994cc7f71a47259c2e53
Róza <rozanagy88@gmail.com>
2019-04-09 17:31:48 +0200

    updated

---
+++ week-02/day-1/src/AppendA.java
@@ -0,0 +1,12 @@
+public class AppendA {
+    public static void main(String[] args) {
+        String[] animals = {"koal", "pand", "zebr"};
+        for (String i : animals) {
+            System.out.println(i + "a");
+        }
+    }
+
+}
+// - Create an array variable named `animals`
+//   with the following content: `["koal", "pand", "zebr"]`
+// - Add all elements an `"a"` at the end
---
+++ week-02/day-2/src/ListIntroduction1.java
@@ -0,0 +1,33 @@
+public class ListIntroduction1 {
+    public static void main(String[] args) {
+        String[] names = {"William", "John", "Amanda"};
+        for(int i = 0;  i < names.length; i++){
+        System.out.println(i + 1 + ". " + names[i]);
+        }
+    }
+
+}
+
+//We are going to play with lists. Feel free to use the built-in methods where possible.
+//
+//Create an empty list which will contain names (strings)
+//Print out the number of elements in the list
+//Add William to the list
+//Print out whether the list is empty or not
+//Add John to the list
+//Add Amanda to the list
+//Print out the number of elements in the list
+//Print out the 3rd element
+//Iterate through the list and print out each name
+//William
+//John
+//Amanda
+//Iterate through the list and print
+//1. William
+//2. John
+//3. Amanda
+//Remove the 2nd element
+//Iterate through the list in a reversed order and print out each name
+//Amanda
+//William
+//Remove all elements
