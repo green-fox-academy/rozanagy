2a22266801d3cd2917c89bb400605bc99b3eeedb
Róza <rozanagy88@gmail.com>
2019-04-25 16:47:09 +0200

    finish CountLetter class

--- week-04/day-4/src/countLetters/CountLetters.java
+++ week-04/day-4/src/countLetters/CountLetters.java
@@ -1,7 +1,49 @@
 package countLetters;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class CountLetters {
-    
+
+    public static void main(String[] args) {
+
+
+        String input = "This is a nice spaceship";
+        HashMap<Character, Integer> map = counterLettersDictionary(input);
+        //Integer i = map.get('a');
+
+        System.out.println(map);
+    }
+
+
+
+        public static HashMap<Character, Integer> counterLettersDictionary (String input){
+
+            HashMap<Character, Integer> dictionary = new HashMap<>();
+            String s1 = input.replaceAll(" ", "");
+            s1 = s1.toLowerCase();
+
+            char[] inputArray = s1.toCharArray();
+            char[] sortedArray = s1.toCharArray();
+            Arrays.sort(input.toCharArray());
+
+            int counter = 0;
+            for (char c : inputArray) {
+                for (char s : sortedArray) {
+                    if (c == s) {
+                        counter++;
+                    }
+                }
+                dictionary.put(c, counter);
+                counter = 0;
+            }
+            return dictionary;
+
+
+        }
+
+
+
 }
 
 //Write a function, that takes a string as an argument
