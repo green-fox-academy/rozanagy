a505a758d9159f815ef014c93740ddee59a93c57
Róza <rozanagy88@gmail.com>
2019-04-25 16:52:15 +0200

    create CountLettersTest class

---
+++ week-04/day-4/tests/countLetters/CountLettersTest.java
@@ -0,0 +1,20 @@
+package countLetters;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CountLettersTest {
+
+    CountLetters countLetters;
+
+    @Before
+    public void setup() {
+        countLetters = new CountLetters();
+    }
+
+    @Test
+    public void counterLettersDictionary() {
+    }
+}
