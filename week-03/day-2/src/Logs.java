9de33f79fbbb300fe955f94fc4ec68f83e297e60
Róza <rozanagy88@gmail.com>
2019-04-25 22:59:56 +0200

    create getIpAddresses method in Logs class

--- week-03/day-2/src/Logs.java
+++ week-03/day-2/src/Logs.java
@@ -1,4 +1,10 @@
+import sun.tools.tree.ArrayAccessExpression;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.nio.file.Path;
@@ -6,17 +12,42 @@ import java.nio.file.Paths;
 
 public class Logs {
     public static void main(String[] args) {
+        Path filePath = Paths.get("assets/log.txt");
+
+        try {
+            System.out.println(getIpAddresses(filePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
-    public static double ratioMethod(String ) {
-        Path
 
+    private static List<String> getIpAddresses(Path filePath) throws IOException {
+        List<String> ipAddresses = new ArrayList<>();
+        List<String> lines = Files.readAllLines(filePath);
+        for(String line : lines) {
+            line.split("\\s+");
+            String[] splitLine = line.split("\\s+");
+            ipAddresses.add(splitLine[5]);
 
         }
 
 
+
+        return ipAddresses;
+
     }
+
+
+    private static float ratioMethod(Path filePath) {
+        return 0.0f;
+
+
+    }
+
+
 }
+
 // Read all data from 'log.txt'.
 // Each line represents a log message from a web server
 // Write a function that returns an array with the unique IP adresses.
