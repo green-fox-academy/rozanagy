4d93ace24df7cfe986bc0928583c310c8b71b4f9
Róza <rozanagy88@gmail.com>
2019-05-15 16:13:18 +0200

    fix SayHelloToAllTheWorld

--- week-06/day-3/src/main/java/com/greenfoxacademy/springstart/controller/SayHelloToAllTheWorld.java
+++ week-06/day-3/src/main/java/com/greenfoxacademy/springstart/controller/SayHelloToAllTheWorld.java
@@ -1,8 +1,23 @@
 package com.greenfoxacademy.springstart.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class SayHelloToAllTheWorld {
-  
+  AtomicLong atomicLong = new AtomicLong();
+
+  String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+      "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+      "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+      "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+  @RequestMapping("/web/sayhello")
+  public String hello(Model model) {
+      model.addAttribute("name", hellos[(int)atomicLong.getAndAdd((1))]);
+    return "sayHello";
+  }
 }
