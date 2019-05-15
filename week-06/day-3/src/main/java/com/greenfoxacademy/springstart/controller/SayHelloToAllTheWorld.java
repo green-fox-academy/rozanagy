4fbfb2c2ede8350344cd5956c2c8ef245800810e
Róza <rozanagy88@gmail.com>
2019-05-15 22:06:37 +0200

    fix SayHelloToAllTheWorld.java

--- week-06/day-3/src/main/java/com/greenfoxacademy/springstart/controller/SayHelloToAllTheWorld.java
+++ week-06/day-3/src/main/java/com/greenfoxacademy/springstart/controller/SayHelloToAllTheWorld.java
@@ -4,11 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 @Controller
 public class SayHelloToAllTheWorld {
-  AtomicLong atomicLong = new AtomicLong();
 
   String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
       "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
@@ -17,7 +14,13 @@ public class SayHelloToAllTheWorld {
 
   @RequestMapping("/web/sayhello")
   public String hello(Model model) {
-      model.addAttribute("name", hellos[(int)atomicLong.getAndAdd((1))]);
+      model.addAttribute("greeting", hellos[greetingSelector()]);
+      model.addAttribute("color", "color: rgb(" + Math.random()*255 + ", " + Math.random()*255 + ", " + Math.random()*255 + ") ;font-size:" + Math.random()*40 + 10 + "px");;
     return "sayHello";
+  }
+
+  private int greetingSelector() {
+    int random = (int) (Math.random() * 50 + 1);
+    return random;
   }
 }
