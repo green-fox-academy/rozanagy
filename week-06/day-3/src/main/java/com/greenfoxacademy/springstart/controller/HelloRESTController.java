31eb15e41f4821da010130923767c7e6fde79671
Róza <rozanagy88@gmail.com>
2019-05-15 12:30:54 +0200

    fix HelloRESTController

--- week-06/day-3/src/main/java/com/greenfoxacademy/springstart/controller/HelloRESTController.java
+++ week-06/day-3/src/main/java/com/greenfoxacademy/springstart/controller/HelloRESTController.java
@@ -4,11 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloRESTController {
+  AtomicLong atomicLong = new AtomicLong();
 
   @RequestMapping("greeting")
   public Greeting greeting(@RequestParam String name) {
-    return new Greeting(1, "Hello " + name + "!");
+    return new Greeting(atomicLong.addAndGet(1), "Hello " + name + "!");
   }
 }
