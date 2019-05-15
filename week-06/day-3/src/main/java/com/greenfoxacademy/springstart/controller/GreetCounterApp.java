cfd11ba2337c9109eee1f6e3372cf3e10cc84d78
Róza <rozanagy88@gmail.com>
2019-05-15 14:57:50 +0200

    fix GreetCounterApp

--- week-06/day-3/src/main/java/com/greenfoxacademy/springstart/controller/GreetCounterApp.java
+++ week-06/day-3/src/main/java/com/greenfoxacademy/springstart/controller/GreetCounterApp.java
@@ -1,4 +1,22 @@
 package com.greenfoxacademy.springstart.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Controller
 public class GreetCounterApp {
+  AtomicLong atomicLong = new AtomicLong();
+
+  @RequestMapping("/web/greetings")
+  public String greetCounter(Model model, @RequestParam String name) {
+    model.addAttribute("name", name);
+    model.addAttribute("atomicLong", atomicLong.getAndAdd(1));
+    return "greetCounter";
+  }
+
+
 }
