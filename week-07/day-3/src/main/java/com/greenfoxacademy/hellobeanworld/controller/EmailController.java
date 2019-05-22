fb729365b08c6120f507fc7d95e281a37fd1ef7a
Róza <rozanagy88@gmail.com>
2019-05-22 17:21:37 +0200

    fix EmailController and email

--- week-07/day-3/src/main/java/com/greenfoxacademy/hellobeanworld/controller/EmailController.java
+++ week-07/day-3/src/main/java/com/greenfoxacademy/hellobeanworld/controller/EmailController.java
@@ -1,23 +1,22 @@
 package com.greenfoxacademy.hellobeanworld.controller;
 
-
 import com.greenfoxacademy.hellobeanworld.service.UtilityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class ColoredController{
+public class EmailController {
 
   @Autowired
   private UtilityService utilityService;
 
-
-  @RequestMapping("/useful/colored")
-  public String colorBackground(Model model) {
-    model.addAttribute("randomColor", utilityService.randomColor());
-    return "color";
+  @RequestMapping("/useful/email")
+  public String emailValidator(Model model, @RequestParam String email) {
+    model.addAttribute("emailAddress", email);
+    model.addAttribute("validEmail", utilityService.validateEmail(email));
+    return "email";
   }
-
 }
