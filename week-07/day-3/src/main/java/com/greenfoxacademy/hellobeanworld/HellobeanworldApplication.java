816b0de542e9d948f63a3f739c938aee8f80aba5
Róza <rozanagy88@gmail.com>
2019-05-22 12:01:24 +0200

    create hellobeanworld

---
+++ week-07/day-3/src/main/java/com/greenfoxacademy/hellobeanworld/HellobeanworldApplication.java
@@ -0,0 +1,28 @@
+package com.greenfoxacademy.hellobeanworld;
+
+import com.greenfoxacademy.hellobeanworld.printer.Printer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobeanworldApplication implements CommandLineRunner {
+
+  private Printer printer;
+
+  @Autowired
+  HellobeanworldApplication(Printer printer) {
+    this.printer = printer;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(HellobeanworldApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    printer.log("hello");
+
+  }
+}
