e6e15aa65b6f0f2de324d4748b4286560b7542ee
Róza <rozanagy88@gmail.com>
2019-05-23 14:29:08 +0200

    fix login.html and MainController

--- week-07/day-4/src/main/java/com/greenfoxacademy/foxclub/controller/MainController.java
+++ week-07/day-4/src/main/java/com/greenfoxacademy/foxclub/controller/MainController.java
@@ -1,7 +1,7 @@
 package com.greenfoxacademy.foxclub.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
@@ -9,6 +9,16 @@ public class MainController {
   @RequestMapping("/")
   public String index() {
     return "index";
+  }
+
+  @GetMapping("/login")
+  public String renderLogin(@RequestParam(required = false) String name) {
+    return "login";
+  }
+
+  @PostMapping("/login")
+  public String namePost(String name) {
+    return "redirect:/?name=" + name;
   }
 
 }
