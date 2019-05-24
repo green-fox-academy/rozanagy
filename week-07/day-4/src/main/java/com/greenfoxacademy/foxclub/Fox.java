28a68eed555ca661386d75b786034cb04d12aa96
Róza <rozanagy88@gmail.com>
2019-05-24 11:45:24 +0200

    fix index.html

--- week-07/day-4/src/main/java/com/greenfoxacademy/foxclub/Fox.java
+++ week-07/day-4/src/main/java/com/greenfoxacademy/foxclub/Fox.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class Fox {
   private String foxName;
-  public ArrayList<String> listOfTricks = new ArrayList<>();
+  private ArrayList<String> listOfTricks = new ArrayList<>();
   private String food;
   private String drink;
 
