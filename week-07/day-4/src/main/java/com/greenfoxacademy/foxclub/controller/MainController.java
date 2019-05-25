2152e43a8184424fd184b346c212b6596697eef1
Róza <rozanagy88@gmail.com>
2019-05-25 20:20:35 +0200

    fix foxclub

--- week-07/day-4/src/main/java/com/greenfoxacademy/foxclub/controller/MainController.java
+++ week-07/day-4/src/main/java/com/greenfoxacademy/foxclub/controller/MainController.java
@@ -4,6 +4,7 @@ import com.greenfoxacademy.foxclub.service.DrinkService;
 import com.greenfoxacademy.foxclub.service.FoodService;
 import com.greenfoxacademy.foxclub.Fox;
 import com.greenfoxacademy.foxclub.service.FoxService;
+import com.greenfoxacademy.foxclub.service.TrickService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,21 +17,32 @@ public class MainController {
   private FoxService foxService;
   private FoodService foodService;
   private DrinkService drinkService;
+  private TrickService trickService;
+
+  public MainController(FoodService foodService, DrinkService drinkService, TrickService trickService) {
+    this.foodService = foodService;
+    this.drinkService = drinkService;
+    this.trickService = trickService;
+  }
 
   @RequestMapping("/")
   public String index(Model model, @RequestParam(required = false) String name) {
     if (name == null) {
       return "login";
+    } else if (foxService.searchForFox(name)) {
+      Fox fox = foxService.findTheFox(name);
+      model.addAttribute("fox", fox);
+      return "index";
     } else {
-      Fox fox = new Fox(name);
+      Fox fox = foxService.findTheFox(name);
       model.addAttribute("fox", fox);
       foxService.addNewFox(fox);
+      return "index";
     }
-    return "index";
   }
 
   @GetMapping("/login")
-  public String renderLogin(@RequestParam(required = false) String name) {
+  public String renderLogin() {
     return "login";
   }
 
@@ -40,7 +52,17 @@ public class MainController {
   }
 
   @RequestMapping("/nutritionStore")
-  public String nutritionSelector(@RequestParam String name) {
-    return name == null ? "login" : "nutrition";
+  public String nutritionSelector(@RequestParam(required = false) String name, Model model) {
+    model.addAttribute("foodList", foodService.findAll());
+    model.addAttribute("drinkList", drinkService.findAll());
+    model.addAttribute("name", name);
+    return "nutrition";
+  }
+
+  @RequestMapping("/tricks")
+  public String trickSelector(@RequestParam(required = false) String name, Model model) {
+    model.addAttribute("trickList", trickService.findAll());
+    model.addAttribute("name", name);
+    return "trick";
   }
 }
